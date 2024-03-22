6f5e563189ff7ad502fa372780dcd3a9d2eb57af
Soudagarseebra <160459423+Soudagarseebra@users.noreply.github.com>
2024-03-22 10:37:37 +0530

    Add files via upload
    CASETOOLS PROJECT :
    
    This is a java based Task Management System that is responsible for adding of task and modifying its completion status and for viewing the list of all the tasks efficiently.
    
    Group Members:
    Shravan Hiremath-2205034
    Hanok John-2205031
    Princeson Fernandes-2205040
    Armaan Sharma-2205015
    Seebra Soudagar-2205005

---
+++ Project-Casetools/src/main/java/com/example/Project/Casetools/ProjectCasetoolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project.Casetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCasetoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCasetoolsApplication.class, args);
+	}
+
+}
